1e0bcf8e66e41cf9d6fac2d87659a3cdea92cb56
pnr <iampnr91@gmail.com>
2021-11-29 21:03:17 +0900

    ..

---
+++ Programmers/src/DevelopmentSpeeds.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class DevelopmentSpeeds {
+
+	public int[] solution(int[] progresses, int[] speeds) { 
+		Queue<Integer> workDate = new LinkedList<>(); //남은 작업시간 
+		List<Integer> result = new ArrayList<Integer>(); //배포갯수
+		
+		 
+		for(int i = 0; i < progresses.length; i++) { 
+			workDate.add((int)Math.ceil(((double)(100 - progresses[i])) / speeds[i]));  
+		} 
+		int max = workDate.poll();  
+		int stack = 1; 
+		
+		for(int i = 1; i < progresses.length; i++) { 
+			if(max < workDate.peek()) { 
+				max = workDate.peek(); 
+				result.add(stack); 
+				stack = 0; 
+			} 
+			workDate.poll(); 
+			stack++; 
+		} 
+		result.add(stack); 
+		
+		//정답 제출 위해 리턴값 변경
+		int []answer = new int[result.size()]; 
+		for(int i=0; i<result.size(); i++) 
+			answer[i] = result.get(i);
+		return answer;
+
+	}
+}
---
+++ Programmers/src/PhoneBook.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class PhoneBook {
+
+	public boolean Solution(String[] phone_book) {
+		boolean answer = true;
+		
+		HashMap<String, Integer> map = new HashMap<>();
+		for(int i=0; i<phone_book.length; i++) {
+			map.put(phone_book[i], i);
+		}
+		
+		for(int i =0; i<phone_book.length; i++) {
+			for(int j=0; j<phone_book[i].length(); j++) {
+				if(map.containsKey(phone_book[i].substring(0, j)))
+					answer = false;
+			}
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		PhoneBook pb = new PhoneBook();
+		String[] pbook = {"119", "97674223", "1195524421"};
+		
+		
+		System.out.println(pb.Solution(pbook));
+	}
+	
+}
