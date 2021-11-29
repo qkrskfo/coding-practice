e1b26076148d4349ea4a4644b0a3a249db8faefb
pnr <iampnr91@gmail.com>
2021-11-29 16:38:10 +0900

    ..

---
+++ ForGugudan.java
@@ -0,0 +1,17 @@
+
+public class ForGugudan {
+	public static void main(String[] args) {
+		for(int i=1; i<10; i++) {
+			for(int j=2; j<10; j++) {
+				System.out.print(j+"x"+i+"="+i*j+" ");
+			}
+			System.out.println();
+		}
+		
+		int a = 2;
+		int b = 4;
+		System.out.println(a<<b);
+		
+		
+	}
+}
