0757f5727c9b81148bdccdb412c16d58126d2622
pnr <iampnr91@gmail.com>
2022-02-20 18:25:10 +0900

    ..

--- 00.baekjun/src/coding/test/Main.java
+++ 00.baekjun/src/coding/test/Main.java
@@ -1,11 +1,34 @@
 package coding.test;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int length = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		
+		for(int i=0; i<length; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())+ "\n");
+		}
+		
+		br.close();
+		
+		bw.flush();
+		bw.close();
+		
+		/*
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		
@@ -14,7 +37,7 @@ public class Main {
 		}
 		sc.close();
 		
-		/*
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
